47145d0035e5f5076cd7c29f669c3ee76952dc1d
DimaKoba <azsx07@rambler.ru>
2021-02-23 19:19:12 +0300

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let strLength = str.length;
+    for (let i = 0; i < strLength; i++) {
+        for (let bracket of bracketsConfig) {
+            if (str.includes(bracket.join(''))) {
+                str = str.replace(bracket.join(''), '');
+                
+            }
+        }
+        strLength--;
+    }
+    return !str.length
 }
+
